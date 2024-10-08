70b97f0567fdde3b5d9b9fbbec1a88ff0458e08f
EMILIA IACOBELLI <eiacobelli@uc.cl>
2024-10-08 15:34:09 -0300

    configuracion cors en app.js

--- api/src/app.js
+++ api/src/app.js
@@ -12,8 +12,12 @@ app.context.orm = orm;
 
 app.use(KoaLogger());
 
-// Usar cors middleware
-app.use(cors());
+// Usar cors middleware con opciones específicas
+app.use(cors({
+  origin: 'https://web.thegoatbet.me',
+  allowMethods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Use bodyParser middleware
 app.use(bodyParser());
@@ -26,3 +30,4 @@ app.use(async (ctx) => {
 });
 
 module.exports = app;
+
